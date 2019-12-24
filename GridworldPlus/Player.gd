1c7410c64184db6381f6cc9b339b80700bfe7166
Markus Feng <markusfeng@gmail.com>
2019-12-24 23:39:11 +0800

    Added player movement action, added health/action bar support

--- GridworldPlus/Player.gd
+++ GridworldPlus/Player.gd
@@ -4,14 +4,34 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
-var x = 0;
-var y = 0;
+var x = 0
+var y = 0
+
+const move_cost = 0.5
+const action_depletion = 1.0
+
+var health = 100
+var action = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+func _process(delta):
+	if action > 0:
+		action -= action_depletion * delta
+	if action < 0:
+		action = 0
 
 func move(world, dx, dy):
-	if world.can_move(self, x + dx, y + dy):
+	if can_move(world, dx, dy):
+		action = move_cost
 		x += dx
 		y += dy
+
+func can_move(world, dx, dy):
+	if action > 0:
+		return false
+	if not world.can_move(self, x + dx, y + dy):
+		return false
+	return true
--- GridworldPlus/PlayerUI.gd
+++ GridworldPlus/PlayerUI.gd
@@ -4,10 +4,18 @@ extends Control
 # var a = 2
 # var b = "text"
 
+export (NodePath) var player
+
+const full_health = 100.0
+const full_action = 5
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if player:
+		var node = get_node(player)
+		$HealthBar.value = node.health * 100 / full_health
+		$ActionBar.value = node.action * 100 / full_action
